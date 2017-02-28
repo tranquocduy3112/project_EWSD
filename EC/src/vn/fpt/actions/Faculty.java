256d4396e4f7a4ef7ed562e9474d376501aa809c
Tran Quoc Duy <tranquocduy3112@gmail.com>
2017-03-01 01:11:00 +0700

    test1

---
+++ EC/src/vn/fpt/actions/Faculty.java
@@ -0,0 +1,11 @@
+package vn.fpt.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Faculty extends ActionSupport {
+
+	public Faculty() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
